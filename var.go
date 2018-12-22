cd39c9f88d7f0fbf1c38ecd59690c8ff82e947e5
Ty-Chen <369494003@qq.com>
2018-12-22 10:50:42 +0800

    Update var.go

--- var.go
+++ var.go
@@ -18,5 +18,48 @@ func vars(){
 	e := 42
 	fmt.Println("e = ", e)
 
+	//指针
+	var ptr *int = &s;
+	fmt.Println("ptr =  ", *ptr)
+
 	fmt.Println("global var = ", globali)
+}
+
+
+func variables() {
+    // 有符号整数，可以表示正负
+    var a int8 = 1 // 1 字节
+    var b int16 = 2 // 2 字节
+    var c int32 = 3 // 4 字节
+    var d int64 = 4 // 8 字节
+    fmt.Println(a, b, c, d)
+
+    // 无符号整数，只能表示非负数
+    var ua uint8 = 1
+    var ub uint16 = 2
+    var uc uint32 = 3
+    var ud uint64 = 4
+    fmt.Println(ua, ub, uc, ud)
+
+    // int 类型，在32位机器上占4个字节，在64位机器上占8个字节
+    var e int = 5
+    var ue uint = 5
+    fmt.Println(e, ue)
+
+    // bool 类型
+    var f bool = true
+    fmt.Println(f)
+
+    // 字节类型
+    var j byte = 'a'
+    fmt.Println(j)
+
+    // 字符串类型
+    var g string = "abcdefg"
+    fmt.Println(g)
+
+    // 浮点数
+    var h float32 = 3.14
+    var i float64 = 3.141592653
+    fmt.Println(h, i)
 }
