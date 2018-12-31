78fb1581d04b72724d7a9bcef815cfc1dd773a8c
Ty-Chen <369494003@qq.com>
2018-12-31 11:22:18 +0800

    Update slice.go

--- slice.go
+++ slice.go
@@ -21,3 +21,28 @@ func initSlice() {
 	fmt.Println(s1, len(s1), cap(s1))
 
 }
+
+func nullSlice() {	
+	var s1 []int
+	var s2 []int = []int{}
+	var s3 []int = make([]int, 0)
+	fmt.Println(s1, s2, s3)
+	fmt.Println(len(s1), len(s2), len(s3))
+	fmt.Println(cap(s1), cap(s2), cap(s3))	
+}
+
+func setSlice() {	
+	var s1 = make([]int, 5, 8)
+	// 切片的访问和数组差不多
+	for i := 0; i < len(s1); i++ {
+	 s1[i] = i + 1
+	}
+	var s2 = s1
+	fmt.Println(s1, len(s1), cap(s1))
+	fmt.Println(s2, len(s2), cap(s2))
+   
+	// 尝试修改切片内容
+	s2[0] = 255
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
