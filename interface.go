bc38a8e1908fb85f9e697af2f59eb11e2ed7bb7f
Ty-Chen <369494003@qq.com>
2019-02-18 23:10:25 +0800

    Update interface.go

--- interface.go
+++ interface.go
@@ -22,6 +22,39 @@ func (a Apple) eat() {
 
 type Flower struct {}
 
+func (f Flower) smell() {
+	fmt.Println("flower can only smell")
+}
+
+func testDiffInterface() {
+	var s1 Smellable
+	var s2 Eatable
+	var apple = Apple{}
+	var flower = Flower{}
+	s1 = apple
+	s1.smell()
+	s1 = flower
+	s1.smell()
+	s2 = apple
+	s2.eat()	
+}
+
+func testEmptyInterface() {
+	// 连续两个大括号，是不是看起来很别扭
+	var user = map[string]interface{}{
+		"age": 30,
+		"address": "Beijing Tongzhou",
+		"married": true,
+	}
+	fmt.Println(user)
+	// 类型转换语法来了
+	var age = user["age"].(int)
+	var address = user["address"].(string)
+	var married = user["married"].(bool)
+	fmt.Println(age, address, married)	
+}
+
 func testInterface()  {
-	
+	testDiffInterface();
+	testEmptyInterface();
 }
