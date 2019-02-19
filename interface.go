d16be902a905125435fa2fa19808980709e39f19
Ty-Chen <369494003@qq.com>
2019-02-19 21:44:04 +0800

    Update interface.go

--- interface.go
+++ interface.go
@@ -40,14 +40,14 @@ func testDiffInterface() {
 }
 
 func testEmptyInterface() {
-	// 连续两个大括号，是不是看起来很别扭
+	
 	var user = map[string]interface{}{
 		"age": 30,
 		"address": "Beijing Tongzhou",
 		"married": true,
 	}
 	fmt.Println(user)
-	// 类型转换语法来了
+	
 	var age = user["age"].(int)
 	var address = user["address"].(string)
 	var married = user["married"].(bool)
