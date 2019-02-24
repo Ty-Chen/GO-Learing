48d03edf8cbc372ce06041f19fe69e43b21ff1b8
Ty-Chen <369494003@qq.com>
2019-02-24 11:41:13 +0800

    Update error.go

--- error.go
+++ error.go
@@ -1,2 +1,19 @@
 package main
 import "fmt"
+
+type error interface {
+	Error() string
+  }
+
+type SomeError struct {
+    Reason string
+}
+
+func (s SomeError) Error() string {
+    return s.Reason
+}
+
+func testError() {
+    var err error = SomeError{"something happened"}
+    fmt.Println(err)
+}
