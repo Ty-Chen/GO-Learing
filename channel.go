fff452e643d8f99f8b85da719df6c857ae20ea27
Ty-Chen <369494003@qq.com>
2019-12-18 23:50:10 +0800

    Update channel.go

--- channel.go
+++ channel.go
@@ -1,11 +1,47 @@
 package main
 import "fmt"
 
-func testChannel() {
+
+func testUnblocking(){
+		messages := make(chan string)
+		signals := make(chan bool)
+	
+		select {
+		case msg := <-messages:
+			fmt.Println("received message", msg)
+		default:
+			fmt.Println("no message received")
+		}
+	
+		msg := "hi"
+		select {
+		case messages <- msg:
+			fmt.Println("sent message", msg)
+		default:
+			fmt.Println("no message sent")
+		}
+	
+		select {
+		case msg := <-messages:
+			fmt.Println("received message", msg)
+		case sig := <-signals:
+			fmt.Println("received signal", sig)
+		default:
+			fmt.Println("no activity")
+		}
+}
+
+
+func testMake() {
 	message := make(chan string)
 	
 	go func() { message <- "ping"}()
 
 	msg:= <- message
 	fmt.Println(msg)
+}
+
+func testChannel(){
+	//testMake();
+	testUnblocking();
 }
