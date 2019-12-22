71c827fc630542ea3b138f9eeaba83e5ee1781e9
Ty-Chen <369494003@qq.com>
2019-12-22 22:09:07 +0800

    Create workerPools.go

---
+++ workerPools.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+    "time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+    for j := range jobs {
+        fmt.Println("worker", id, "started  job", j)
+        time.Sleep(time.Second)
+        fmt.Println("worker", id, "finished job", j)
+        results <- j * 2
+    }
+}
+
+func worker2(id int, wg *sync.WaitGroup){
+	fmt.Printf("Worker %d starting\n", id)
+
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+
+	wg.Done()
+}
+
+func testWorkerPools() {
+
+    jobs := make(chan int, 100)
+    results := make(chan int, 100)
+
+    for w := 1; w <= 3; w++ {
+        go worker(w, jobs, results)
+    }
+
+    for j := 1; j <= 5; j++ {
+        jobs <- j
+    }
+    close(jobs)
+
+    for a := 1; a <= 5; a++ {
+        <-results
+    }
+}
+
+func testWaitGroups() {
+	
+}
