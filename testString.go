77c63a409270c3c4aaaca12eb72407b1e77cda1c
Ty-Chen <369494003@qq.com>
2020-01-04 12:00:02 +0800

    Update testString.go

--- testString.go
+++ testString.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     s "strings"
+    "os"
 )
 
 var p = fmt.Println
@@ -25,4 +26,58 @@ func testString() {
 
     p("Len: ", len("hello"))
     p("Char:", "hello"[1])
+}
+
+type point struct {
+    x, y int
+}
+
+func testStringFormat() {
+
+    p := point{1, 2}
+    fmt.Printf("%v\n", p)
+
+    fmt.Printf("%+v\n", p)
+
+    fmt.Printf("%#v\n", p)
+
+    fmt.Printf("%T\n", p)
+
+    fmt.Printf("%t\n", true)
+
+    fmt.Printf("%d\n", 123)
+
+    fmt.Printf("%b\n", 14)
+
+    fmt.Printf("%c\n", 33)
+
+    fmt.Printf("%x\n", 456)
+
+    fmt.Printf("%f\n", 78.9)
+
+    fmt.Printf("%e\n", 123400000.0)
+    fmt.Printf("%E\n", 123400000.0)
+
+    fmt.Printf("%s\n", "\"string\"")
+
+    fmt.Printf("%q\n", "\"string\"")
+
+    fmt.Printf("%x\n", "hex this")
+
+    fmt.Printf("%p\n", &p)
+
+    fmt.Printf("|%6d|%6d|\n", 12, 345)
+
+    fmt.Printf("|%6.2f|%6.2f|\n", 1.2, 3.45)
+
+    fmt.Printf("|%-6.2f|%-6.2f|\n", 1.2, 3.45)
+
+    fmt.Printf("|%6s|%6s|\n", "foo", "b")
+
+    fmt.Printf("|%-6s|%-6s|\n", "foo", "b")
+
+    s := fmt.Sprintf("a %s", "string")
+    fmt.Println(s)
+
+    fmt.Fprintf(os.Stderr, "an %s\n", "error")
 }
